9ee44e89ef77dc39b32e1f71f48ab2093198b713
Tom <tomchabas1@gmail.com>
2023-05-17 14:42:30 +0200

    add home

--- assets/js/views/Home.vue
+++ assets/js/views/Home.vue
@@ -1,17 +1,46 @@
 <template>
-    <Headermenu />
+    <div class="containermain">
+        <div class="textleft">
+            <h1>La plateforme de vote la plus en vogue de l'EFREI</h1>
+        </div>
+        <div class="imageright">
+            <img src="../../images/imageAccueil.png" class="img-fluid" width="680" height="364"/>
+        </div>
+    </div>
 </template>
 
 <script>
-    import Headermenu from '../components/Headermenu.vue';
 
     export default {
-        name: 'Home',
-        components: {
-            Headermenu
-        }
+        name: 'Home'
     }
 </script>
     
 <style scoped>
+    .containermain{
+        display: flex;
+        flex-direction: row;
+        margin: 5% 10% 0 10%;
+        align-items: center;
+        justify-content: space-around;
+    }
+
+    .textleft{
+        display: flex;
+        flex-shrink: 1;
+        flex-basis: 50%;
+        color: #3c7ba0;
+        justify-content: center;
+        text-align: center;
+    }
+    .textleft h1{
+        display: flex;
+        width: 60%;
+        justify-content: center;
+        font-style: normal;
+        font-weight: 600;
+        font-size: 48px;
+        line-height: 59px;
+        text-align: center;
+    }
 </style>
